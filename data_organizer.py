5e4c1cafcf7390d4d3e77936361c422c3fc0107f
vishnuchakr <vishnuc2@illinois.edu>
2019-04-16 17:24:55 -0500

    Wrote a method to find the number of images in a folder.

--- data_organizer.py
+++ data_organizer.py
@@ -2,6 +2,8 @@ import os, os.path
 import shutil
 
 def file_size(input_file):
-	
+	file = os.listdir(input_file)
+	file_size = len(file)
+	return file_size;	
 
 def file_organizer(is_training, input_file, target_file):
