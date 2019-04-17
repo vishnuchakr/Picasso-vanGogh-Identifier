587b4b71c35918f61c86c1194d063558428f0073
vishnuchakr <vishnuc2@illinois.edu>
2019-04-16 20:10:59 -0500

    Wrote a method to move the images into the appropriate training or validation directories.

--- data_organizer.py
+++ data_organizer.py
@@ -1,9 +1,26 @@
 import os, os.path
 import shutil
+import glob
 
-def file_size(input_file):
-	file = os.listdir(input_file)
-	file_size = len(file)
-	return file_size;	
+#Returns the length of the input file.
+def folder_size(input_folder):
+	folder = os.listdir(input_folder)
+	size = len(folder)
+	return size;	
 
-def file_organizer(is_training, input_file, target_file):
+#Transfers the appropriate image files to the designated target folder.
+def file_organizer(is_training, input_folder, target_folder):
+	if is_training:
+		total_length = file_size(input_folder)
+		seventy_percent = int(0.7 * total_length)
+		for file in glob.glob(input_folder):
+			if seventy_percent > 0:
+				seventy_percent -= 1
+				shutil.move(file, target_folder)
+			else:
+				break
+
+	else:
+		for file in glob.glob(input_folder):
+			shutil.move(file,target_folder)
+
