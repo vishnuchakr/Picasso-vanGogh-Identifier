bd9e64bedb1451aea24f69500983dffd7f01eb29
vishnuchakr <vishnuc2@illinois.edu>
2019-04-16 23:39:59 -0500

    Executed script to organize image data.

--- data_organizer.py
+++ data_organizer.py
@@ -1,6 +1,5 @@
 import os, os.path
 import shutil
-import glob
 
 #Returns the length of the input file.
 def folder_size(input_folder):
@@ -9,28 +8,31 @@ def folder_size(input_folder):
 	return size;	
 
 #Transfers the appropriate image files to the designated target folder.
-def file_organizer(is_training, input_folder, target_folder):
-	if is_training:
-		total_length = file_size(input_folder)
-		seventy_percent = int(0.7 * total_length)
-		for file in glob.glob(input_folder):
-			if seventy_percent > 0:
-				seventy_percent -= 1
-				shutil.move(file, target_folder)
-			else:
-				break
+def folder_organizer(is_training, input_folder, target_folder):
+    files = os.listdir(input_folder)
+    print(files)
+    if is_training:
+        seventy_percent = int(0.7 * folder_size(input_folder))
+        for file in files:
+            file_path = os.path.join(input_folder, file)
+            if seventy_percent > 0:
+                print(file)
+                seventy_percent -= 1
+                shutil.move(file_path, target_folder)
+            else:
+                break
+    else:
+        for file in files:
+            file_path = os.path.join(input_folder, file)
+            shutil.move(file_path, target_folder)
 
-	else:
-		for file in glob.glob(input_folder):
-			shutil.move(file,target_folder)
-
-picasso_dataset = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/Picasso"
-vangogh_dataset = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/vanGogh"
-training_picasso = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/training/Picasso"
-training_vangogh = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/training/vanGogh"
-validation_picasso = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/validation/Picasso"
-validation_vangogh = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/validation/vanGogh"
-file_organizer(True, picasso_dataset, training_picasso)
-file_organizer(True, vangogh_dataset, training_vangogh)
-file_organizer(False, picasso_dataset, validation_picasso)
-file_organizer(False, vangogh_dataset, validation_vangogh)
+picasso_dataset = os.path.join('C:/', 'Users', 'vishn', 'skinIO_project', 'Picasso-vanGogh-Identifier', 'data', 'Picasso')
+vangogh_dataset = os.path.join('C:/', 'Users', 'vishn', 'skinIO_project', 'Picasso-vanGogh-Identifier', 'data', 'vanGogh')
+training_picasso = os.path.join('C:/', 'Users', 'vishn', 'skinIO_project', 'Picasso-vanGogh-Identifier', 'data', 'training', 'Picasso')
+training_vangogh = os.path.join('C:/', 'Users', 'vishn', 'skinIO_project', 'Picasso-vanGogh-Identifier', 'data', 'training', 'vanGogh')
+validation_picasso = os.path.join('C:/', 'Users', 'vishn', 'skinIO_project', 'Picasso-vanGogh-Identifier', 'data', 'validation', 'Picasso')
+validation_vangogh = os.path.join('C:/', 'Users', 'vishn', 'skinIO_project', 'Picasso-vanGogh-Identifier', 'data', 'validation', 'Picasso')
+folder_organizer(True, picasso_dataset, training_picasso)
+folder_organizer(True, vangogh_dataset, training_vangogh)
+folder_organizer(False, picasso_dataset, validation_picasso)
+folder_organizer(False, vangogh_dataset, validation_vangogh)
