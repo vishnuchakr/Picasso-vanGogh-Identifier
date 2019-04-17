c5f40eee06b04d267934d078ecc98a8ac6680430
vishnuchakr <vishnuc2@illinois.edu>
2019-04-16 20:26:56 -0500

    Finished script to organize data.

--- data_organizer.py
+++ data_organizer.py
@@ -24,3 +24,13 @@ def file_organizer(is_training, input_folder, target_folder):
 		for file in glob.glob(input_folder):
 			shutil.move(file,target_folder)
 
+picasso_dataset = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/Picasso"
+vangogh_dataset = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/vanGogh"
+training_picasso = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/training/Picasso"
+training_vangogh = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/training/vanGogh"
+validation_picasso = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/validation/Picasso"
+validation_vangogh = "C:/Users/vishn/skinIO_project/Picasso-vanGogh-Identifier/data/validation/vanGogh"
+file_organizer(True, picasso_dataset, training_picasso)
+file_organizer(True, vangogh_dataset, training_vangogh)
+file_organizer(False, picasso_dataset, validation_picasso)
+file_organizer(False, vangogh_dataset, validation_vangogh)
