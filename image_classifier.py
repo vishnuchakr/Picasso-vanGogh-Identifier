8d73afbc5028587e0b377a259f13d9e6a00edc7d
vishnuchakr <vishnuc2@illinois.edu>
2019-04-17 00:13:19 -0500

    Created an image classifier from keras.

---
+++ image_classifier.py
@@ -0,0 +1,39 @@
+#Import the required packages
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+class LeNet:
+
+	@staticmethod
+	def build(width, height, depth, classes):
+		#Construct the model
+		model = Sequential()
+		inputShape = (height, width, depth)
+
+		#First set of CONV -> RELU -> POOL layers
+		model.add(Conv2D(20, (5, 5), padding="same",
+			input_shape = inputShape))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+		#Second set of CONV -> RELU -> POOL layers
+		model.add(Conv2D(50, (5, 5), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size(2, 2), strides=(2, 2)))
+
+		#Set of FC -> RELU layers
+		model.add(Flatten())
+		model.add(Dense(500))
+		model.add(Activation("relu"))
+
+		#Softmax classifier
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+
+		#Return the constructed architecture
+		return model
